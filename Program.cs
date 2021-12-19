77eb76bc65614671467c8d44fedf16ee6a4939a4
emreakgun45 <emreakgun455@gmail.com>
2021-12-20 00:10:53 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace sinif_kavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(Gunler.Pazar);
+            System.Console.WriteLine((int)Gunler.Cumartesi);
+
+            int sicaklik = 32;
+
+            if(sicaklik <= (int)(HavaDurumu.Normal))
+                System.Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısnmasını bekleyelim.");
+            else if(sicaklik >= (int)(HavaDurumu.Sicak))
+                System.Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün.");
+            else if(sicaklik >= (int)(HavaDurumu.Normal) && sicaklik <= (int)(HavaDurumu.Sicak))
+                System.Console.WriteLine("Hadi dışarıya çıkalım.");
+        }
+
+    }
+
+    enum Gunler
+    {
+        Pazartesi = 1,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma=23,
+        Cumartesi,
+        Pazar
+    }
+
+    enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal = 20,
+        Sicak = 25,
+        CokSicak = 30
+    }
+
+}
